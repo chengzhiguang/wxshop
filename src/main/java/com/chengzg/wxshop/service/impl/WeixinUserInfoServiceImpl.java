991601aad04719f9f1e8ae33ea865e96f9a1f06b
chengzg3 <chengzg3.ziroom.com>
2017-11-16 09:34:21 +0800

    初始化项目1

--- src/main/java/com/chengzg/wxshop/service/impl/WeixinUserInfoServiceImpl.java
+++ src/main/java/com/chengzg/wxshop/service/impl/WeixinUserInfoServiceImpl.java
@@ -1,8 +1,12 @@
 package com.chengzg.wxshop.service.impl;
 
+import com.chengzg.wxshop.entity.WxUserinfo;
+import com.chengzg.wxshop.enums.DelType;
+import com.chengzg.wxshop.mapper.WxUserinfoMapper;
 import com.chengzg.wxshop.model.vo.WeiXinUser;
 import com.chengzg.wxshop.service.IWeixinUserInfoService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,47 +17,43 @@ public class WeixinUserInfoServiceImpl implements IWeixinUserInfoService {
 
 	private static Logger logger = Logger.getLogger(WeixinUserInfoServiceImpl.class);
 
+	@Autowired
+	private WxUserinfoMapper wxUserinfoMapper;
+
 	@Override
 	public Integer saveWxUserinfo(WeiXinUser weiXinUser) {
 		try {
-			/*//1 根据openId从数据库中获取
-			//不存在就添加
-			//存在就修改，并修改删除标识
-			OilWxUserinfo oilWxUserinfo = new OilWxUserinfo();
-			oilWxUserinfo.setWxOpenId(weiXinUser.getOpenid());
-			List<OilWxUserinfo> oilWxUserinfoList = oilWxUserinfoDao.queryOilWxUserinfos(oilWxUserinfo);
-			if(oilWxUserinfoList == null) {
-				logger.error("saveWxUserinfo 查询微信用户信息错误。oilWxUserinfoList为null");
+			if (weiXinUser == null) {
 				return -1;
 			}
-			if(oilWxUserinfoList.size() > 0) {//已经存在
-				OilWxUserinfo oilWxUserinfo2 = oilWxUserinfoList.get(0);
-				oilWxUserinfo2.setIsDel(DelType.NO_DEL.getIndex());
-				oilWxUserinfo2.setLastModifyTime(new Date());
-				oilWxUserinfo2.setWxHeadPic(weiXinUser.getHeadimgurl());
-				oilWxUserinfo2.setWxNikeName(weiXinUser.getNickname());
-				oilWxUserinfo2.setWxOpenId(weiXinUser.getOpenid());
-				oilWxUserinfo2.setWxCity(weiXinUser.getCity());
-				oilWxUserinfo2.setWxCountry(weiXinUser.getCountry());
-				oilWxUserinfo2.setWxLanguage(weiXinUser.getLanguage());
-				oilWxUserinfo2.setWxProvince(weiXinUser.getProvince());
-				oilWxUserinfo2.setWxSex(weiXinUser.getSex());
-				oilWxUserinfoDao.modifyOilWxUserinfoById(oilWxUserinfo2);
-			} else {//不存在
-				OilWxUserinfo oilWxUserinfo2 = new OilWxUserinfo();
-				oilWxUserinfo2.setCrateTime(new Date());
-				oilWxUserinfo2.setIsDel(DelType.NO_DEL.getIndex());
-				oilWxUserinfo2.setLastModifyTime(new Date());
-				oilWxUserinfo2.setWxHeadPic(weiXinUser.getHeadimgurl());
-				oilWxUserinfo2.setWxNikeName(weiXinUser.getNickname());
-				oilWxUserinfo2.setWxOpenId(weiXinUser.getOpenid());
-				oilWxUserinfo2.setWxCity(weiXinUser.getCity());
-				oilWxUserinfo2.setWxCountry(weiXinUser.getCountry());
-				oilWxUserinfo2.setWxLanguage(weiXinUser.getLanguage());
-				oilWxUserinfo2.setWxProvince(weiXinUser.getProvince());
-				oilWxUserinfo2.setWxSex(weiXinUser.getSex());
-				oilWxUserinfoDao.addOilWxUserinfo(oilWxUserinfo2);
-			}*/
+			WxUserinfo wxUserinfo = wxUserinfoMapper.selectByWxOpenId(weiXinUser.getOpenid());
+			if (wxUserinfo == null) {
+				wxUserinfo = new WxUserinfo();
+				wxUserinfo.setCrateTime(new Date());
+				wxUserinfo.setIsDel(DelType.NO.getIndex());
+				wxUserinfo.setLastModifyTime(new Date());
+				wxUserinfo.setWxHeadPic(weiXinUser.getHeadimgurl());
+				wxUserinfo.setWxNikeName(weiXinUser.getNickname());
+				wxUserinfo.setWxOpenId(weiXinUser.getOpenid());
+				wxUserinfo.setWxCity(weiXinUser.getCity());
+				wxUserinfo.setWxCountry(weiXinUser.getCountry());
+				wxUserinfo.setWxLanguage(weiXinUser.getLanguage());
+				wxUserinfo.setWxProvince(weiXinUser.getProvince());
+				wxUserinfo.setWxSex(weiXinUser.getSex());
+				wxUserinfoMapper.insertSelective(wxUserinfo);
+			} else {
+				wxUserinfo.setIsDel(DelType.NO.getIndex());
+				wxUserinfo.setLastModifyTime(new Date());
+				wxUserinfo.setWxHeadPic(weiXinUser.getHeadimgurl());
+				wxUserinfo.setWxNikeName(weiXinUser.getNickname());
+				wxUserinfo.setWxOpenId(weiXinUser.getOpenid());
+				wxUserinfo.setWxCity(weiXinUser.getCity());
+				wxUserinfo.setWxCountry(weiXinUser.getCountry());
+				wxUserinfo.setWxLanguage(weiXinUser.getLanguage());
+				wxUserinfo.setWxProvince(weiXinUser.getProvince());
+				wxUserinfo.setWxSex(weiXinUser.getSex());
+				wxUserinfoMapper.updateByPrimaryKeySelective(wxUserinfo);
+			}
 		} catch (Exception e) {
 			logger.error("saveWxUserinfo 异常", e);
 		}
